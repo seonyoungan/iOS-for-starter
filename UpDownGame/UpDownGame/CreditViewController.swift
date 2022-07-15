dde87ed5c2ba982d4936444c92b99450ef74af48
seonyoungan <asy093@naver.com>
2022-07-16 00:53:59 +0900

    Lesson6 Add Credit view

---
+++ UpDownGame/UpDownGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 안선영 on 2022/07/16.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUPDismissButton(_ sender: UIButton){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
